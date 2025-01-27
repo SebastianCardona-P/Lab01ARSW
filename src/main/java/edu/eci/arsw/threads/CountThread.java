73669768dc2a3079b632db0525bb2398d9eb5bfa
SebastianCardona-P <sebastian.cardona-p@mail.escuelaing.edu.co>
2025-01-27 14:01:30 -0500

    ADD: Part 1 Threads java

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -9,6 +9,30 @@ package edu.eci.arsw.threads;
  *
  * @author hcadavid
  */
-public class CountThread {
+public class CountThread extends Thread{
+
+    private int a;
+    private int b;
+
+    public CountThread(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public void run(){
+        String threadName = Thread.currentThread().getName();
+        for (int i = a; i <= b; i++){
+            System.out.println( threadName + " " +i);
+        }
+    }
+
+    public int getA(){
+        return a;
+    }
+
+    public int getB(){
+        return b;
+    }
     
 }
